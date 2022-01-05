74d17d04da289b01eec7dafbc0542de8e267490a
Guilherme Martins Lopes <guilhermeml@itera.com.br>
2022-01-05 14:09:50 -0300

    fix: setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='logstash',
-    version='1.0.1',
+    version='1.0.0',
     packages=find_packages(),
-    install_requires=['elastisearch'],
+    install_requires=['elasticsearch'],
     url='',
     license='',
     author='Guilherme ML',
